da73a4b44c5086361384f7c5c767c1079fe32334
ntvorup <nach0004@stud.kea.dk>
2025-04-01 09:02:36 +0200

    1. commit

---
+++ src/main/java/org/example/wishlistapp/WishlistAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.wishlistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistAppApplication.class, args);
+    }
+
+}
