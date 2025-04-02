a3bd87650889caf90b0720949c24c8553a954453
nial0003 <nial0003@stud.kea.dk>
2025-04-03 00:51:39 +0200

    Added the abstract methods to the Repository class so it fulfill the contract with the IRepository Interface

--- src/main/java/org/example/wishlistapp/repository/Repository.java
+++ src/main/java/org/example/wishlistapp/repository/Repository.java
@@ -1,4 +1,20 @@
 package org.example.wishlistapp.repository;
 
-public class Repository {
+import java.util.List;
+
+public abstract class Repository<T> implements IRepository<T> {
+    @Override
+    public abstract void addToDatabase(T newObjectToAdd);
+
+    @Override
+    public abstract void deleteFromDatabase(T objectToDelete) ;
+
+    @Override
+    public abstract T findById(int id);
+
+    @Override
+    public abstract List<T> getAll();
+
+    @Override
+    public abstract Boolean edit(T newObject);
 }
