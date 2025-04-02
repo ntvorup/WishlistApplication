fb669649b01c3233521d930423c93e638200e073
Daniel-Platz <dani0005@stud.kea.dk>
2025-04-02 09:58:41 +0200

    Changed name of WishListAlreadyExistsException to WishAlreadyExistsException

--- src/main/java/org/example/wishlistapp/exceptions/wishlist/WishAlreadyExistsException.java
+++ src/main/java/org/example/wishlistapp/exceptions/wishlist/WishAlreadyExistsException.java
@@ -1,10 +1,10 @@
 package org.example.wishlistapp.exceptions.wishlist;
 
-public class WishListAlreadyExistsException extends WishlistException {
+public class WishAlreadyExistsException extends WishlistException {
 
     private final String identifier;
 
-    public WishListAlreadyExistsException(String identifier) {
+    public WishAlreadyExistsException(String identifier) {
         super("Wishlist with identifier " + identifier + " already exists");
         this.identifier = identifier;
     }
