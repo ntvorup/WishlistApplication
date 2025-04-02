1e297b9337f251105b182dc1034e4b8bb7f283ae
nial0003 <nial0003@stud.kea.dk>
2025-04-03 00:44:25 +0200

    Added all the methods in the interface, created the abstract Repository class that will implement the interface methods

--- src/main/java/org/example/wishlistapp/repository/IRepository.java
+++ src/main/java/org/example/wishlistapp/repository/IRepository.java
@@ -1,4 +1,11 @@
 package org.example.wishlistapp.repository;
 
-public interface IRepository {
+import java.util.List;
+
+public interface IRepository<T> {
+    void addToDatabase(T newObjectToAdd);
+    void deleteFromDatabase(T objectToDelete);
+    T findById (int id);
+    List<T> getAll();
+    Boolean edit(T newObject);
 }
